65a2fa0b3a6da0477a25e66f0196cff5adceb484
ROUNAK GUPTA <rounakg866@gmail.com>
2025-08-01 17:43:18 +0530

    custom api response and error handling

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
+import app from './app.js';
 dotenv.config();
 const PORT = process.env.PORT || 3000;
-const app = express();
 connectDB()
 .then(()=>{
     app.listen(PORT || 3000 , () =>{
